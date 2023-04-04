85ae1e36ed72df22cc1b58b70b104f9b04bddea6
arunreddypadala <arunreddypadala@gmail.com>
2023-04-04 19:59:36 -0400

    Adding A9 server code

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 import express from 'express';
 import cors from 'cors'
+import mongoose from "mongoose";
+// mongoose.connect('mongodb://localhost:27017/tuiter');
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+                          || 'mongodb://127.0.0.1:27017/tuiter'
+mongoose.connect(CONNECTION_STRING);
+
+
 import HelloController
     from "./controllers/hello-controller.js"
 import UserController
@@ -14,3 +21,4 @@ HelloController(app)
 UserController(app)
 TuitsController(app)
 app.listen(process.env.PORT || 4000);
+
